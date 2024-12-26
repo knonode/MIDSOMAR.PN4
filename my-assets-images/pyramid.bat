ea222daa941c065c81e2b9519af5d97f58ca4c89
knonode <algohmp@gmail.com>
2024-12-26 03:32:11 +0100

    Mobile optimization of 'About.html' and 'Tracker.html'. Added bottom menu bar and side menu in About.

---
+++ my-assets-images/pyramid.bat
@@ -0,0 +1,11 @@
+@echo off
+magick montage ^
+00_midsomar_thumb.jpg ^
+01_midsomar_thumb.jpg 02_midsomar_thumb.jpg ^
+03_midsomar_thumb.jpg 04_midsomar_thumb.jpg 05_midsomar_thumb.jpg ^
+06_midsomar_thumb.jpg 07_midsomar_thumb.jpg 08_midsomar_thumb.jpg 09_midsomar_thumb.jpg ^
+10_midsomar_thumb.jpg 11_midsomar_thumb.jpg 12_midsomar_thumb.jpg 13_midsomar_thumb.jpg 14_midsomar_thumb.jpg ^
+15_midsomar_thumb.jpg 16_midsomar_thumb.jpg 17_midsomar_thumb.jpg 18_midsomar_thumb.jpg 19_midsomar_thumb.jpg 20_midsomar_thumb.jpg ^
+21_midsomar_thumb.jpg 22_midsomar_thumb.jpg 23_midsomar_thumb.jpg 24_midsomar_thumb.jpg 25_midsomar_thumb.jpg 26_midsomar_thumb.jpg 27_midsomar_thumb.jpg ^
+28_midsomar_thumb.jpg 29_midsomar_thumb.jpg 30_midsomar_thumb.jpg 31_midsomar_thumb.jpg 32_midsomar_thumb.jpg 33_midsomar_thumb.jpg 34_midsomar_thumb.jpg 35_midsomar_thumb.jpg ^
+-tile 8x8 -geometry 15%%x15%%+0+0 -gravity center pyramid_midsomar.jpg
